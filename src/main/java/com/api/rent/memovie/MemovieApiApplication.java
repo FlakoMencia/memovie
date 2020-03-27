37001114f7ce1e759283d10d0dd7ce947561ff83
MarioMencia <flakomencia@gmail.com>
2020-03-26 23:27:30 -0600

    test-Heroku

--- src/main/java/com/api/rent/memovie/MemovieApiApplication.java
+++ src/main/java/com/api/rent/memovie/MemovieApiApplication.java
@@ -3,6 +3,7 @@ package com.api.rent.memovie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MemovieApiApplication {
@@ -10,10 +11,14 @@ public class MemovieApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemovieApiApplication.class, args);
     }
-    
-    @RequestMapping({"/"})
-    public String helloTest(){
-        return "PRIMERA PRUEBA DE AMBIENTE CON HEROKU";
+
+    @RestController
+    class pruebaParaHeroku {
+        @RequestMapping({"/"})
+        public String helloTest() {
+            return "PRIMERA PRUEBA DE AMBIENTE CON HEROKU";
+        }
+
     }
 
 }
