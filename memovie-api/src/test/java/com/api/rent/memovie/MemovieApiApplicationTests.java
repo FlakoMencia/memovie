084a1a57675d5e632c7426be69c88cbba9888dba
MarioMencia <flakomencia@gmail.com>
2020-03-26 21:30:59 -0600

    Test commit for Heroku configuration

---
+++ memovie-api/src/test/java/com/api/rent/memovie/MemovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rent.memovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
